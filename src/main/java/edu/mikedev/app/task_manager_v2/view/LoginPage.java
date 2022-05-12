9d0a17cdb0b09a3b9eab324f661d98eb9b3d88a5
mikedev <mik3dev@gmail.com>
2022-05-12 17:01:17 +0200

    add login page tests

---
+++ src/main/java/edu/mikedev/app/task_manager_v2/view/LoginPage.java
@@ -0,0 +1,6 @@
+package edu.mikedev.app.task_manager_v2.view;
+
+import javax.swing.*;
+
+public class LoginPage extends JPanel {
+}
---
+++ src/test/java/edu/mikedev/app/task_manager_v2/view/LoginPageTest.java
@@ -0,0 +1,37 @@
+package edu.mikedev.app.task_manager_v2.view;
+
+import static org.junit.Assert.*;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.*;
+
+public class LoginPageTest {
+
+	FrameFixture window;
+
+	@Before
+	public void setUp() throws Exception {
+		  JFrame frame = GuiActionRunner.execute(() -> {
+			 JFrame jframe = new JFrame();
+			 jframe.setContentPane(new LoginPage());
+			 return jframe;
+		  });
+		  window = new FrameFixture(frame);
+		  window.show(); // shows the frame to test
+	}
+
+	@Test
+	public void testLoginPageComponents() {
+		window.label("lblUsername").requireText("Username");
+		window.textBox("txtUsername").requireEmpty();
+		window.label("lblPassword").requireText("Password");
+		window.textBox("txtPassword").requireEmpty();
+		window.button("btnLogin").requireText("Login");
+		window.button("btnRegister").requireText("Register");
+	}
+
+}
