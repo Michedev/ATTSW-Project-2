5400d4c8a2314a8620c46d276b7eea1a1ea46a2d
mikedev <mik3dev@gmail.com>
2022-05-19 17:26:11 +0200

    add tests to get user task by id in Model

--- src/main/java/edu/mikedev/app/task_manager_v2/model/HibernateModel.java
+++ src/main/java/edu/mikedev/app/task_manager_v2/model/HibernateModel.java
@@ -1,5 +1,6 @@
 package edu.mikedev.app.task_manager_v2.model;
 
+import edu.mikedev.app.task_manager_v2.data.Task;
 import edu.mikedev.app.task_manager_v2.data.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -21,5 +22,9 @@ public class HibernateModel {
         User logged = repository.getUserByUsernamePassword(username, password);
         session.close();
         return logged;
+    }
+
+    public Task getUserTask(int id) {
+        return null;
     }
 }
--- src/test/java/edu/mikedev/app/task_manager_v2/model/HibernateModelTest.java
+++ src/test/java/edu/mikedev/app/task_manager_v2/model/HibernateModelTest.java
@@ -1,5 +1,6 @@
 package edu.mikedev.app.task_manager_v2.model;
 
+import edu.mikedev.app.task_manager_v2.data.Task;
 import edu.mikedev.app.task_manager_v2.data.User;
 import edu.mikedev.app.task_manager_v2.utils.HibernateDBUtilsInMemory;
 import org.hibernate.SessionFactory;
@@ -15,6 +16,10 @@ public class HibernateModelTest {
     private static HibernateDBUtilsInMemory dbUtils;
     private static SessionFactory sessionFactory;
     private HibernateModel model;
+    private final String USERNAME = "username1";
+    private final String PASSWORD = "password1";
+    private User user;
+
 
     @BeforeClass
     public static void setUpClass(){
@@ -31,18 +36,40 @@ public class HibernateModelTest {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        user = dbUtils.users.stream().filter(u -> u.getUsername().equals(USERNAME) && u.getPassword().equals(PASSWORD)).findFirst().get();
+
     }
 
     @Test
     public void testModelLogin(){
-        final String USERNAME = "username1";
-        String PASSWORD = "password1";
-        User expected = dbUtils.users.stream().filter(u -> u.getUsername().equals(USERNAME) && u.getPassword().equals(PASSWORD)).findFirst().get();
         User actual = model.login(USERNAME, PASSWORD);
 
-        Assert.assertEquals(expected.getId(), actual.getId());
-        Assert.assertEquals(expected.getUsername(), actual.getUsername());
-        Assert.assertEquals(expected.getPassword(), actual.getPassword());
-        Assert.assertEquals(expected.getEmail(), actual.getEmail());
+        Assert.assertEquals(user.getId(), actual.getId());
+        Assert.assertEquals(user.getUsername(), actual.getUsername());
+        Assert.assertEquals(user.getPassword(), actual.getPassword());
+        Assert.assertEquals(user.getEmail(), actual.getEmail());
     }
+
+    @Test
+    public void testModelGetTaskLoggedUser(){
+        model.login(USERNAME, PASSWORD);
+
+        Task expected = user.getTasks().iterator().next();
+
+        Task actual = model.getUserTask(expected.getId());
+
+        Assert.assertEquals(expected.getId(), actual.getId());
+        Assert.assertEquals(expected.getTitle(), actual.getTitle());
+        Assert.assertEquals(expected.getSubtask1(), actual.getSubtask1());
+        Assert.assertEquals(expected.getSubtask2(), actual.getSubtask2());
+        Assert.assertEquals(expected.getSubtask3(), actual.getSubtask3());
+    }
+
+    @Test(expected = IllegalAccessException.class)
+    public void testModelGetTaskUnloggedUser(){
+        Task expected = user.getTasks().iterator().next();
+
+        model.getUserTask(expected.getId());
+    }
+
 }
