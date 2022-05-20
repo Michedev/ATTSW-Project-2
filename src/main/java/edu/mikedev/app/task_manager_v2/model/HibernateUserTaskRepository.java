c288ad945ca69f13c490d271e91769696583df6b
mikedev <mik3dev@gmail.com>
2022-05-20 10:44:44 +0200

    simplified implementation of get[User|Task]ById

--- src/main/java/edu/mikedev/app/task_manager_v2/model/HibernateUserTaskRepository.java
+++ src/main/java/edu/mikedev/app/task_manager_v2/model/HibernateUserTaskRepository.java
@@ -16,38 +16,31 @@ public class HibernateUserTaskRepository implements UserTaskRepository {
     public void add(User user){
         session.save(user);
     }
-
     @Override
     public void delete(User user) {
         User persistentUser = session.find(User.class, user.getId());
         session.delete(persistentUser);
     }
-
     @Override
     public void update(Task task) {
         session.update(task);
     }
-
     @Override
     public User getUserById(int id) {
-        return session.createQuery(String.format("Select u from User u where u.id = %d", id), User.class).getSingleResult();
+        return session.find(User.class, id);
     }
-
     @Override
     public Task getTaskById(int id) {
-        return session.createQuery(String.format("Select t from Task t where t.id = %d", id), Task.class).getSingleResult();
+        return session.find(Task.class, id);
     }
-
     @Override
     public void add(Task task) {
         session.save(task);
     }
-
     @Override
     public void delete(Task toDelete) {
         session.delete(toDelete);
     }
-
     @Override
     public User getUserByUsernamePassword(String username, String password) {
         return session.createQuery(String.format("SELECT u FROM User u where u.username = '%s' and u.password = '%s'", username, password), User.class).getSingleResult();
