a080e7dc6e4c8723b71162319e64458befb61626
mikedev <mik3dev@gmail.com>
2022-05-26 10:20:30 +0200

    add test correct registration flow

--- src/test/java/edu/mikedev/app/task_manager_v2/controller/RegisterControllerTest.java
+++ src/test/java/edu/mikedev/app/task_manager_v2/controller/RegisterControllerTest.java
@@ -1,14 +1,19 @@
 package edu.mikedev.app.task_manager_v2.controller;
 
+import edu.mikedev.app.task_manager_v2.data.User;
 import edu.mikedev.app.task_manager_v2.model.Model;
 import edu.mikedev.app.task_manager_v2.view.LoginPage;
 import edu.mikedev.app.task_manager_v2.view.RegisterPage;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -111,6 +116,31 @@ public class RegisterControllerTest {
         verify(registerPage, times(1))
                 .setErrorLabelUsername("Username must contain only alphanumeric characters");
 
+    }
+
+    @Test
+    public void testCorrectRegistration(){
+        String registrationUsername = "username";
+        String registrationPassword = "aa";
+        String registrationEmail = "email@email.it";
+        when(registerPage.getUsername()).thenReturn(registrationUsername);
+        when(registerPage.getPassword()).thenReturn(registrationPassword);
+        when(registerPage.getEmail()).thenReturn(registrationEmail);
+
+        registerController.onRegisterButtonClick();
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+
+        verify(mainController, times(1)).setViewController(any(LoginController.class));
+        verify(model, times(1)).registerUser(userCaptor.capture());
+
+        List<User> capturedUsers = userCaptor.getAllValues();
+        Assert.assertEquals(1, capturedUsers.size());
+        User capturedUser = capturedUsers.get(0);
+
+        Assert.assertEquals(registrationUsername, capturedUser.getUsername());
+        Assert.assertEquals(registrationPassword, capturedUser.getPassword());
+        Assert.assertEquals(registrationEmail, capturedUser.getEmail());
     }
 
 }
