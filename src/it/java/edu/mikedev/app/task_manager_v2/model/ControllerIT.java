83391581da1f50d5455e4815c4243ce0f956401a
mikedev <mik3dev@gmail.com>
2022-05-27 12:23:24 +0200

    add tests to go into detail page and to make a new task

--- src/it/java/edu/mikedev/app/task_manager_v2/model/ControllerIT.java
+++ src/it/java/edu/mikedev/app/task_manager_v2/model/ControllerIT.java
@@ -6,6 +6,7 @@ import edu.mikedev.app.task_manager_v2.data.User;
 import edu.mikedev.app.task_manager_v2.view.LoginPage;
 import edu.mikedev.app.task_manager_v2.view.NewUpdateTask;
 import edu.mikedev.app.task_manager_v2.view.RegisterPage;
+import edu.mikedev.app.task_manager_v2.view.TaskDetail;
 import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
@@ -19,6 +20,7 @@ import org.mockito.Mock;
 
 import javax.swing.*;
 
+import java.awt.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -100,6 +102,106 @@ public class ControllerIT extends AssertJSwingJUnitTestCase {
         window.button("btnConfirm").click();
 
         verify(mockedModel).registerUser(any(User.class));
+    }
+
+    @Test
+    @GUITest
+    public void testViewDetailPage() throws PermissionException {
+        Set<Task> tasks = new HashSet<>();
+        Task task1 = new Task("A", "B", "C", "D");
+        task1.setId(1);
+        Task task2 = new Task("123", "1", "2", "3");
+        task2.setId(2);
+        tasks.add(task1);
+        tasks.add(task2);
+
+        String username = "user123";
+        String password = "pass123";
+        User newUser = new User(username, password, "email@email.it", tasks);
+        when(mockedModel.login(username, password)).thenReturn(newUser);
+
+        window.textBox("txtUsername").enterText(username);
+        window.textBox("txtPassword").enterText(password);
+        window.button("btnLogin").click();
+
+        window.button("btnDetailTask1").click();
+
+        Container contentPane = jframe.getContentPane();
+        Assert.assertTrue(contentPane instanceof TaskDetail);
+
+        TaskDetail taskDetail = (TaskDetail) contentPane;
+        Assert.assertEquals(task1, taskDetail.getTask());
+
+        window.label("lblTaskTitle").requireText(task1.getTitle());
+    }
+
+    @Test
+    @GUITest
+    public void testGoTONewTaskPage() throws PermissionException {
+        Set<Task> tasks = new HashSet<>();
+        Task task1 = new Task("A", "B", "C", "D");
+        task1.setId(1);
+        Task task2 = new Task("123", "1", "2", "3");
+        task2.setId(2);
+        tasks.add(task1);
+        tasks.add(task2);
+
+        String username = "user123";
+        String password = "pass123";
+        User newUser = new User(username, password, "email@email.it", tasks);
+        when(mockedModel.login(username, password)).thenReturn(newUser);
+
+        window.textBox("txtUsername").enterText(username);
+        window.textBox("txtPassword").enterText(password);
+        window.button("btnLogin").click();
+
+        window.button("btnNew").click();
+
+        Assert.assertTrue(jframe.getContentPane() instanceof NewUpdateTask);
+    }
+
+    @Test
+    @GUITest
+    public void testNewTask() throws PermissionException {
+        Set<Task> tasks = new HashSet<>();
+        Task task1 = new Task("A", "B", "C", "D");
+        task1.setId(1);
+        Task task2 = new Task("123", "1", "2", "3");
+        task2.setId(2);
+        tasks.add(task1);
+        tasks.add(task2);
+
+        String username = "user123";
+        String password = "pass123";
+        User newUser = new User(username, password, "email@email.it", tasks);
+        when(mockedModel.login(username, password)).thenReturn(newUser);
+
+        window.textBox("txtUsername").enterText(username);
+        window.textBox("txtPassword").enterText(password);
+        window.button("btnLogin").click();
+
+        window.button("btnNew").click();
+
+        String newTaskTitle = "AAA";
+        String newTaskSubtask1 = "1";
+        String newTaskSubtask2 = "2";
+        String newTaskSubtask3 = "3";
+        window.textBox("txtTaskTitle").enterText(newTaskTitle);
+        window.textBox("txtStep1").enterText(newTaskSubtask1);
+        window.textBox("txtStep2").enterText(newTaskSubtask2);
+        window.textBox("txtStep3").enterText(newTaskSubtask3);
+
+        window.button("btnMake").click();
+
+        ArgumentCaptor<Task> captor = ArgumentCaptor.forClass(Task.class);
+        verify(mockedModel).addUserTask(captor.capture());
+
+        Task addedTask = captor.getValue();
+
+        Assert.assertEquals(newTaskTitle, addedTask.getTitle());
+        Assert.assertEquals(newTaskSubtask1, addedTask.getSubtask1());
+        Assert.assertEquals(newTaskSubtask2, addedTask.getSubtask2());
+        Assert.assertEquals(newTaskSubtask3, addedTask.getSubtask3());
     }
 
 }
