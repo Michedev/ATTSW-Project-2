107d762cb30d7a69fde068e480439058be383465
mikedev <mik3dev@gmail.com>
2022-06-07 15:40:54 +0200

    add missing database table creation on postgresql

--- src/main/resources/initdb.sql
+++ src/main/resources/initdb.sql
@@ -1,3 +1,19 @@
-DROP DATABASE IF EXISTS test;
+CREATE TABLE IF NOT EXISTS Users (
+    id serial PRIMARY KEY,
+	username varchar(255),
+	password varchar(255),
+	email varchar(255)
+);
 
-CREATE DATABASE test;
+
+
+
+CREATE TABLE IF NOT EXISTS Tasks (
+    id serial PRIMARY KEY,
+	title varchar(255),
+	subtask1 varchar(255),
+	subtask2 varchar(255),
+	subtask3 varchar(255),
+	id_user int,
+	FOREIGN KEY (id_user) REFERENCES Users (id)
+);
